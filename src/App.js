073b894d4073c2764a975a6ce5801b584af47139
Lemcur <rafal.klimek@netguru.co>
2020-07-02 14:55:13 +0200

    basic routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import './App.css';
 import Login from './components/Login.js'
 import Index from './components/cruds/Index.js'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Index />
+      <Router>
+        <ul>
+          <li>
+            <Link to="/login">login</Link>
+          </li>
+          <li>
+            <Link to="/cruds">cruds</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/cruds">
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
